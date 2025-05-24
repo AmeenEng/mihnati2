a8af4ab91d5874613ec75ae164c47c406b3dd6a9
Ameen <ameenalalimi.dev@gmail.com>
2025-05-24 23:39:43 +0300

    adding the translations files

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:mihnati2/Onboarding/onboarding_view.dart';
 import 'package:mihnati2/auth/login.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:easy_localization/easy_localization.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+      supportedLocales: [Locale('en'), Locale('ar')],
+      path: 'assets/translations',
+      fallbackLocale: Locale('ar'),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,11 +25,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
         theme: ThemeData(
           fontFamily: 'Cairo',
         ),
-        debugShowCheckedModeBanner: false,
-        home: FutureBuilder(
+        home: FutureBuilder<bool>(
           future: isOnboardingCompleted(),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.waiting) {
