b31ea5a88b8bb1593f8b36bd4a98765d410e718b
Ameen <ameenalalimi.dev@gmail.com>
2025-05-31 22:55:42 +0300

    save changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:mihnati2/auth/services/firebase_auth_methods.dart';
 import 'package:provider/provider.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'auth/auth_provider.dart';
